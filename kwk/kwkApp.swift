23210a699acd9de7aea90840228bde008043f8fb
catalinavt <71410774+catalinavt@users.noreply.github.com>
2021-06-29 12:37:42 -0500

    Initial Commit

---
+++ kwk/kwkApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkApp.swift
+//  kwk
+//
+//  Created by Florence on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct kwkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
